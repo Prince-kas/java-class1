ae0eeff411350466edb0318dbf1f37fbad007c28
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:35:00 +0530

    Create Stringcount.java

---
+++ questions/Stringcount.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class stringcount {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String s1="refrigerator";
+        System.out.println(s1.length());
+
+    } 
+    
+}
