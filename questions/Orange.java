433edb60d57c465bc0c6aa7efefb269537559069
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:33:29 +0530

    Create Orange.java

---
+++ questions/Orange.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class orange{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1="This is orange juice";
+        int l=s1.length();
+        for(int i=0; i<l; i++){
+            char ch=s1.charAt(i);
+            System.out.println(ch);
+
+        }
+
+    }
+}
