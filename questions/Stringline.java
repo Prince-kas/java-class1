b8fd97f17901d08023a1381aa5c0c85f27689310
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:36:09 +0530

    Create Stringline.java

---
+++ questions/Stringline.java
@@ -0,0 +1,11 @@
+import java.util.*;
+    public class stringline {
+        public static void main(String[] args){
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter a string");
+            String str=sc.nextLine();
+            System.out.println("String="+str);
+    
+        }
+    }
+   
