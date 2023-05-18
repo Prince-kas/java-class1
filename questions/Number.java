b43ba51a82d341d045a158d96fdef93950f2e350
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:32:28 +0530

    Create Number.java

---
+++ questions/Number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class number{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter a number");
+    int a=sc.nextInt();
+    
+    if(a%2==0){
+        System.out.println("even=");
+    
+}
+else {
+    System.out.println("odd");
+}
+}
+}
