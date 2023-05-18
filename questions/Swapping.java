d5be32fdf757ba4a8332c8af855d9fd248665d15
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:37:57 +0530

    Create Swapping.java

---
+++ questions/Swapping.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Swapping {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a,b,t;
+        System.out.println("Enter two numbers");
+        a=sc.nextInt();
+        b=sc.nextInt();
+        t=a;
+        a=b;
+        t=b;
+        System.out.println("+a");
+        System.out.println("+b");
+
+        
+    }
+    
+}
