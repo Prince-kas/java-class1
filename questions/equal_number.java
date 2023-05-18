bbf4783f3bc26e4deaf0fba5a5c0f220332fab7a
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-18 11:28:01 +0530

    Create equal_number.java

---
+++ questions/equal_number.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class equal_number {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter two numbers");
+        int a =sc.nextInt();
+        int b= sc.nextInt();
+
+        if(a==b){
+            System.out.println("equal numbers");
+             }
+             else {
+                System.out.println("not equal");
+             }
+
+    }
+
+    
+}
