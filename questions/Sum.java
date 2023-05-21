e9ccebbd1632797b4075bae8b4732cba219cc37f
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-21 20:55:07 +0530

    Add files via upload

---
+++ questions/Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+    public static void main(String[] args){
+        int n=6;
+
+        int sum=0; 
+        for(int i=0; i<=n; i++){
+            sum=sum+i;
+
+        }
+        System.out.println("sum="+sum);
+
+    }
+    
+}
