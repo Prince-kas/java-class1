df4cca5a5d54827a30e85707a90e53f9b36c7a34
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-21 20:57:26 +0530

    Create sumA.java

---
+++ questions/sumA.java
@@ -0,0 +1,13 @@
+public class SumA {
+    public static void main(String[]args){
+        int sum=0;
+        int number []= {97,98,65,78,};
+        int size=4;
+        for (int i=0; i<size; i++){
+            sum=sum+ number[i];
+
+        }
+        System.out.println(sum);
+
+    }
+}
