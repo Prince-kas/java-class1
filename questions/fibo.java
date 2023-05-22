61e17d4b2ae95b5d8beaebb61f774ad6b2fb914f
Prince kashyap <132996809+Prince-kas@users.noreply.github.com>
2023-05-22 22:37:03 +0530

    Create fibo.java

---
+++ questions/fibo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Fibo {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int n1=0;
+        int n2=1;
+        int n3;
+        System.out.println(n1+""+n2);
+        for(int i=2; i<=n; i++){
+            n3=n1+n2;
+            System.out.println(""+n3);
+            n1=n2;
+            n2=n3;
+
+        }
+    }
+    
+}
